a32233ade2b2be568b3656b97512a71072ed39c0
user <user@DESKTOP-T9TKTM2>
2021-06-13 14:57:19 +0900

    커밋

---
+++ src/main/java/egovframework/healthper/vo/UserVO.java
@@ -0,0 +1,29 @@
+package egovframework.healthper.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserVO {
+	
+	private String m_idx;
+	private String m_name;
+	private String m_id;
+	private String m_pw;
+	private String m_phone;
+	private String m_birth;
+	private String m_sex;
+	private String m_type;
+	private String m_place;
+	private String m_div;
+	private String m_favorit_g;
+	private String m_profile_url;
+	private String m_matching;
+	private String m_email; 
+	private String m_etc;
+	private String m_place_idx;
+	
+}
